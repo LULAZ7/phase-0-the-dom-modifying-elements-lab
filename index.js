d63945ba67d24b88ba8e7ccc68843ccae74cb8e6
Lula <loulazakaria02@gmail.com>
2022-12-03 13:16:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementsId('main');
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Hasbi is the champion";
+
+
+
+
